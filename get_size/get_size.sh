91e2657a507f7fff175ed8d1f9850d81df53be82
Wbl <baolong.wang@westwell-lab.com>
2020-07-14 16:30:06 +0800

    fix get_size

--- get_size/get_size.sh
+++ get_size/get_size.sh
@@ -4,7 +4,7 @@ files=`find ./ | awk -F "\." '{print $NF}' | grep -v "/" | sort -u`
 for file in $files
 do
     suffer="\."$file"$"
-    find ./ | grep "$suffer" |xargs wc -c |awk -v file=$file 'BEGIN{count=0;size=0;} \
-        {count = count + 1; size = size + $1/1024;} \
-        END{print size/1024/2 "M  " count " " file}'
+    find ./ | grep "$suffer" |xargs du -b |awk -v file=$file 'BEGIN{count=0;size=0;} \
+        {count = count + 1; size = size + $1;} \
+        END{print size/1024/1024 "M  " count " " file}'
 done
